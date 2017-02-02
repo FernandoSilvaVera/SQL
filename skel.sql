0eeffeb9274fc5b3d0116d71ec7d14ac008e24d1
Fernando Silva <fernandosilv4c@gmail.com>
2017-02-02 20:34:57 +0100

    skel mysql

---
+++ skel.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS *****;
+
+	CREATE DATABASE ******;
+
+	USE *****
+
+DROP TABLE IF EXIST -----;
+
+	CREATE TABLE -----(
+	// ????? VARCHAR (X) 
+
+
+	//PRIMARY KEY (/////)
+	//NOT NULL -->es necesario rellenar el valor
+	//NULL --> No es necesario rellenar el valor
+	//DEFAULT 'X' --> Valor por defecto
+        //AUTO_INCREMENT --> Incrementa de forma automatica	
+
+
+	);
+
+INSERT INTO -----(?????,¿¿¿¿¿¿)
+
+	VALUES ('','','');
+
+
