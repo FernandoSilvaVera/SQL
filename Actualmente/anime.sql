e1cb131611eed9d4f27b518959a0b45cbb09855e
Fernando Silva <fernandosilv4c@gmail.com>
2017-06-02 22:26:30 +0200

    Base de datos anime

---
+++ Actualmente/anime.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS ANIME;
+
+	CREATE DATABASE ANIME;
+
+	USE ANIME
+
+DROP TABLE IF EXISTS ANIMES;
+
+CREATE TABLE ANIMES(
+
+	nombre VARCHAR (50) NOT NULL,
+  	tag VARCHAR(50) NOT NULL,
+	nota TINYINT NOT NULL,
+	capitulos SMALLINT NOT NULL,
+	PRIMARY KEY(nombre)
+
+);
+
+CREATE TABLE FECHA(
+
+	nombre_anime VARCHAR(50) NOT NULL,
+	mes_inicio set('Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre') NOT NULL,
+	mes_final set('Enero','Febrero''Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre','En emision') NOT NULL,
+	temporada set('Invierno','Primavera','Verano','Otoño') NOT NULL,
+	año CHAR(4) NOT NULL,
+	dia_nuevo_cap set('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo','Finalizado')NOT NULL,
+	PRIMARY KEY (nombre_anime),
+	FOREIGN KEY (nombre_anime)
+		REFERENCES ANIMES (nombre)
+		ON UPDATE CASCADE ON DELETE CASCADE
+
+);
+
+CREATE TABLE GENERO(
+
+	nombre_anime VARCHAR(50) NOT NULL,
+	principal VARCHAR(30) NOT NULL,
+	secundario VARCHAR(30) NULL,
+	PRIMARY KEY (nombre_anime),
+	FOREIGN KEY (nombre_anime)
+		REFERENCES ANIMES (nombre)
+		ON UPDATE CASCADE ON DELETE CASCADE
+
+);
+
+CREATE TABLE CHICAS(
+
+	nombre_anime VARCHAR(50) NOT NULL,
+	nombre VARCHAR(30) NOT NULL,
+	color_pelo VARCHAR(30) NOT NULL,
+	loli CHAR (2) NOT NULL,
+	PRIMARY KEY (nombre),
+	FOREIGN KEY (nombre_anime)
+		REFERENCES ANIMES (nombre)
+		ON UPDATE CASCADE ON DELETE CASCADE
+
+);
+
