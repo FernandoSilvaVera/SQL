b57df8a1cd10a08667dbf3db7b8968d0139e50c2
Fernando Silva <fernandosilv4c@gmail.com>
2017-06-02 23:08:34 +0200

    Datos de anime

---
+++ Actualmente/datos.sql
@@ -0,0 +1,66 @@
+insert into ANIMES VALUES("Zero kara hajimeru mahou no sho","1","Bruja rengar blanco","10","8");
+insert into ANIMES VALUES("Rokudenashi majutsu koushi to akashic records","1","Best uniforme of anime","10","8");
+insert into ANIMES VALUES("Oushitsu kyoushi Haine","1","Tutor real","9","7");
+insert into ANIMES VALUES("Saenai heroine no sodatekata 2","2","Crear juego","8","12");
+insert into ANIMES VALUES("Clockwork Planet","1","Mundo de relojes","10","12");
+insert into ANIMES VALUES("Shingeki no kyojin 2","2","Mikasa","10","12");
+insert into ANIMES VALUES("Boku no hero 2","2","All Might","10","12");
+insert into ANIMES VALUES("Eromanga-sensei","1","Hermana dibuja ero","9","12");
+insert into ANIMES VALUES("Re:Creators","1","Fuera de su anime","7","12");
+insert into ANIMES VALUES("Alice to Zoroku","1","lolis poderes","10","12");
+insert into ANIMES VALUES("Busou shoujo machiavellianism","1","peli verde super kawaii","10","12");
+
+insert into FECHA VALUES("Zero kara hajimeru mahou no sho","Abril","En emision","Primavera","2017","Lunes");
+insert into FECHA VALUES("Rokudenashi majutsu koushi to akashic records","Abril","En emision","Primavera","2017","Martes");
+insert into FECHA VALUES("Oushitsu kyoushi Haine","Abril","En emision","Primavera","2017","Martes");
+insert into FECHA VALUES("Saenai heroine no sodatekata 2","Abril","En emision","Primavera","2017","Jueves");
+insert into FECHA VALUES("Clockwork Planet","Abril","En emision","Primavera","2017","Jueves");
+insert into FECHA VALUES("Shingeki no kyojin 2","Abril","En emision","Primavera","2017","Sabado");
+insert into FECHA VALUES("Boku no hero 2","Abril","En emision","Primavera","2017","Sabado");
+insert into FECHA VALUES("Eromanga-sensei","Abril","En emision","Primavera","2017","Sabado");
+insert into FECHA VALUES("Re:Creators","Abril","En emision","Primavera","2017","Sabado");
+insert into FECHA VALUES("Alice to Zoroku","Abril","En emision","Primavera","2017","Sabado");
+insert into FECHA VALUES("Busou shoujo machiavellianism","Abril","En emision","Primavera","2017","Domingo");
+/*
+insert into GENERO VALUES("Accel World","Comedia","Accion");
+*/
+insert into CHICAS VALUES("Zero kara hajimeru mahou no sho","Zero","Plateado","no");
+insert into CHICAS VALUES("Zero kara hajimeru mahou no sho","Albus","Rubio","no");
+insert into CHICAS VALUES("Rokudenashi majutsu koushi to akashic records","Sistine Fibel","Plateado","no");
+insert into CHICAS VALUES("Rokudenashi majutsu koushi to akashic records","Rubio Tingel","Rubio","no");
+insert into CHICAS VALUES("Rokudenashi majutsu koushi to akashic records","Re=L Rayford","Azul","si");
+insert into CHICAS VALUES("Rokudenashi majutsu koushi to akashic records","Celica Arfonia","Rubio","no");
+insert into CHICAS VALUES("Oushitsu kyoushi Haine","Adele von Glanzreich","Rubio","si");
+insert into CHICAS VALUES("Saenai heroine no sodatekata 2","Eriri Spencer Sawamura","Rubio","no");
+insert into CHICAS VALUES("Saenai heroine no sodatekata 2","Utaha Kasumigaoka","Negro","no");
+insert into CHICAS VALUES("Saenai heroine no sodatekata 2","Megumi Katou","Castaño","no");
+insert into CHICAS VALUES("Saenai heroine no sodatekata 2","Hyoudou Michiru","Morado","no");
+insert into CHICAS VALUES("Clockwork Planet","RyuZU","Plateado","no");
+insert into CHICAS VALUES("Clockwork Planet","Marie Bell Breguest","Rubio","si");
+insert into CHICAS VALUES("Clockwork Planet","AnchoR","Rubio","si");
+insert into CHICAS VALUES("Shingeki no kyojin 2","Ymir","Negro","no");
+insert into CHICAS VALUES("Shingeki no kyojin 2","MiKasa Ackerman","Negro","no");
+insert into CHICAS VALUES("Shingeki no kyojin 2","Historia Reiss","Rubio","no");
+insert into CHICAS VALUES("Shingeki no kyojin 2","Annie Leonhart","Rubio","no");
+insert into CHICAS VALUES("Shingeki no kyojin 2","Hanji Zoe","Castaño","no");
+insert into CHICAS VALUES("Shingeki no kyojin 2","Sasha Blouse","Castaño","no");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
