3eac1639765cbd31f44092877b769045e3149d64
Fernando Silva <fernandosilv4c@gmail.com>
2017-07-02 15:18:03 +0200

    Quitando fallos

--- Actualmente/datos.sql
+++ Actualmente/datos.sql
@@ -8,28 +8,28 @@ insert into ANIMES VALUES("Clockwork Planet","1","Mundo de relojes","10","12");
 insert into ANIMES VALUES("Shingeki no kyojin 2","2","Mikasa","10","12");
 insert into ANIMES VALUES("Boku no hero 2","2","All Might","10","12");
 insert into ANIMES VALUES("Eromanga-sensei","1","Hermana dibuja ero"," 9","12");
-insert into ANIMES VALUES("Re:Creators",																				"1",			"Fuera de su anime",					" 7",		"12");
-insert into ANIMES VALUES("Alice to Zoroku",																		"1",			"lolis poderes",							"10",		"12");
-insert into ANIMES VALUES("Busou shoujo machiavellianism",											"1",			"peli verde super kawaii",		"10",		"12");
+insert into ANIMES VALUES("Re:Creators","1","Fuera de su anime"," 7","12");
+insert into ANIMES VALUES("Alice to Zoroku","1","lolis poderes","10","12");
+insert into ANIMES VALUES("Busou shoujo machiavellianism","1","peli verde super kawaii","10","12");
 
-insert into ANIMES VALUES("Accel World",																				"1",			"Prota cerdito",							" 7",		"12");
-insert into ANIMES VALUES("Akame ga kill",																			"1",			"Prota no es prota",					" 7",		"12");
-insert into ANIMES VALUES("Akiba`s strip",																			"1",			"Quitar ropa para ganar",			" 8",		"12");
-insert into ANIMES VALUES("Angel Beats",																				"1",			"Loli programadora",					" 8",		"12");
-insert into ANIMES VALUES("Another",																						"1",			"Chica parche",								"10",		"12");
-insert into ANIMES VALUES("Assassination Classroom",														"1",			"Koro-sensei",								" 9",		"24");
-insert into ANIMES VALUES("Assassination Classroom 2",													"2",			"Koro-sensei",								" 8",		"24");
-insert into ANIMES VALUES("Baby princess 3d",																		"1",			"harem 1 capitulo",						" 7",		" 1");
-insert into ANIMES VALUES("Black Bullet",																				"1",			"lolis para todos",						" 7",		" 1");
-insert into ANIMES VALUES("Boku dake ga inai machi",														"1",			"Volver al pasado",						" 9",		" 1");
-insert into ANIMES VALUES("Boku no hero",																				"1",			"All Might",									"10",		"12");
-insert into ANIMES VALUES("Charlotte",																					"1",			"Chicos con poderes",					" 8",		"13");
-insert into ANIMES VALUES("Choujingen game neptune",														"1",			"Diosas lolis",								"10",		"13");
-insert into ANIMES VALUES("Date a live",																				"1",			"Preguntas",									" 7",		"12");
-insert into ANIMES VALUES("Date a live 2",																			"2",			"Kurumi",											" 8",		"12");
-insert into ANIMES VALUES("Death parade",																				"1",			"Pruebas muerte",							" 9",		"12");
-insert into ANIMES VALUES("Kono subarashii",																		"1",			"Diosa inutil",								"10",		"10");
-insert into ANIMES VALUES("Kono subarashii 2",																	"2",			"Rival megumi",								"10",		"10");
+insert into ANIMES VALUES("Accel World","1","Prota cerdito"," 7","12");
+insert into ANIMES VALUES("Akame ga kill","1","Prota no es prota"," 7","12");
+insert into ANIMES VALUES("Akiba`s strip","1","Quitar ropa para ganar"," 8","12");
+insert into ANIMES VALUES("Angel Beats","1","Loli programadora"," 8","12");
+insert into ANIMES VALUES("Another","1","Chica parche","10","12");
+insert into ANIMES VALUES("Assassination Classroom","1","Koro-sensei"," 9","24");
+insert into ANIMES VALUES("Assassination Classroom 2","2","Koro-sensei"," 8","24");
+insert into ANIMES VALUES("Baby princess 3d","1","harem 1 capitulo"," 7"," -1");
+insert into ANIMES VALUES("Black Bullet","1","lolis para todos"," 7"," 1");
+insert into ANIMES VALUES("Boku dake ga inai machi","1","Volver al pasado"," 9"," 1");
+insert into ANIMES VALUES("Boku no hero","1","All Might","10","12");
+insert into ANIMES VALUES("Charlotte","1","Chicos con poderes",	" 8","13");
+insert into ANIMES VALUES("Choujingen game neptune","1","Diosas lolis","10","13");
+insert into ANIMES VALUES("Date a live","1","Preguntas"," 7","12");
+insert into ANIMES VALUES("Date a live 2","2","Kurumi"," 8","12");
+insert into ANIMES VALUES("Death parade","1","Pruebas muerte"," 9","12");
+insert into ANIMES VALUES("Kono subarashii","1","Diosa inutil","10","10");
+insert into ANIMES VALUES("Kono subarashii 2","2","Rival megumi","10","10");
 
 
 
@@ -62,6 +62,7 @@ insert into PERSONAJES VALUES("Mujer","Rokudenashi majutsu koushi to akashic rec
 insert into PERSONAJES VALUES("Mujer","Rokudenashi majutsu koushi to akashic records","Celica Arfonia","Rubio","no");
 insert into PERSONAJES VALUES("Mujer","Oushitsu kyoushi Haine","Adele von Glanzreich","Rubio","si");
 insert into PERSONAJES VALUES("Mujer","Saenai heroine no sodatekata 2","Eriri Spencer Sawamura","Rubio","no");
+
 insert into PERSONAJES VALUES("Mujer","Saenai heroine no sodatekata 2","Utaha Kasumigaoka","Negro","no");
 insert into PERSONAJES VALUES("Mujer","Saenai heroine no sodatekata 2","Megumi Katou","Castaño","no");
 insert into PERSONAJES VALUES("Mujer","Saenai heroine no sodatekata 2","Hyoudou Michiru","Morado","no");
@@ -80,7 +81,7 @@ insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Tsuyu Asui","Verde","no"
 insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Kyoka Jiro","Morado","no");
 insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Mina Ashido","Rosa","no");
 insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Toru Hagakure","Invisible","no");
-insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Itsuka Kendō","Naranja","no");
+insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Itsuka Kendo","Naranja","no");
 insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Ibara Shiozaki","Verde","no");
 insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Recovery Girl","Gris","no");
 insert into PERSONAJES VALUES("Mujer","Boku no hero 2","Mei Hatsume","Rosa","no");
@@ -95,7 +96,7 @@ insert into PERSONAJES VALUES("Mujer","Re:Creators","Selesia Yupitiria","Rojo","
 insert into PERSONAJES VALUES("Mujer","Re:Creators","Meteora Osterreich","Plateado","no");
 insert into PERSONAJES VALUES("Mujer","Re:Creators","Alicetelia February","Rubio","no");
 insert into PERSONAJES VALUES("Mujer","Re:Creators","Mamika Kirameki","Rosa","si");
-insert into PERSONAJES VALUES("Mujer","Re:Creators","Magane Chikujōin","Morado","no");
+insert into PERSONAJES VALUES("Mujer","Re:Creators","Magane Chikujoin","Morado","no");
 insert into PERSONAJES VALUES("Mujer","Re:Creators","Altair","Plateado","no");
 insert into PERSONAJES VALUES("Mujer","Re:Creators","Marine","Castaño","no");
 insert into PERSONAJES VALUES("Mujer","Alice to Zoroku","Sana Kashimura","Rubio","si");
@@ -112,7 +113,7 @@ insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Warabi Ha
 insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Satori Tamaba","Verde","si");
 insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Tsukuyo Inaba","Plateado","si");
 insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Nono Mozunono","Rosa","si");
-insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Chōka U. Barazaki","Rosa","si");
+insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Choka U. Barazaki","Rosa","si");
 insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Sasa Kurasaki","Plateado","si");
 insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Ui Migii","Azul","si");
 insert into PERSONAJES VALUES("Mujer","Busou shoujo machiavellianism","Tsunemi Toko","Castaño","si");
