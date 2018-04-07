8120a8867dbbfbc0db78f2a404748d5bc6763cb9
ShuviDola <fernandosilv4c@gmail.com>
2018-04-07 16:25:58 +0200

    old data

---
+++ Anime/laravel/old/load.sql
@@ -0,0 +1,6 @@
+source ./animes.sql;
+source ./characters.sql;
+source ./dates.sql;
+source ./genres.sql;
+source ./genre_animes.sql;
+source ./videos.sql;
---
+++ Anime/laravel/old/videos.sql
@@ -0,0 +1,1 @@
+insert into videos values('18if', '13', 'https://content-na.drive.amazonaws.com/cdproxy/templink/odghliP-WxpIuT6ERnpF9fIYIYF1XFUjTzqlTt0EYPUeJxFPc');
